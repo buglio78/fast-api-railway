bb35d2227bbe2502d3e986b52bf30c655ee5de8f
buglio78 <buglio1978@gmail.com>
2025-07-14 22:50:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Ciao! FastAPI funziona su Railway 🚂"}
+    return {"message": "🚂 FastAPI Railway è attivo!"}
 
-@app.get("/step/{step_id}")
-def get_step(step_id: int):
-    return {"step_id": step_id, "status": "In lavorazione"}
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Ciao, {name}!"}
